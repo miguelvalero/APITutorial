84fc6891e7d4bf20083d0c3c4d6d7fc767a215c3
Miguel Valero <miguel.valero@upc.edu>
2018-12-26 10:19:58 +0100

    API recien creada

---
+++ src/models/persona.model.ts
@@ -0,0 +1,33 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Persona extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  pass: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  rol: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  puntos: number;
+
+  constructor(data?: Partial<Persona>) {
+    super(data);
+  }
+}
